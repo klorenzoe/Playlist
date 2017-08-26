7e7a2d7617334eeee5789e8e38e60e2a08a63225
katherine Lorenzo <krle_10@hotmail.com>
2017-08-25 19:33:46 -0600

    first commit

---
+++ Laboratorio0_1016816/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio0_1016816
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Laboratorio0_1016816/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio0_1016816.MvcApplication" Language="C#" %>
