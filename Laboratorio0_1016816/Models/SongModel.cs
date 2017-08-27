f86faa38d21fa99ae3568e2a92f2839b001ff42a
katherine Lorenzo <krle_10@hotmail.com>
2017-08-27 17:29:40 -0600

    models and tools folder added.

---
+++ Laboratorio0_1016816/Models/SongModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Laboratorio0_1016816.Models
+{
+    public class SongModel
+    {
+        [Key]
+        [Required]
+        [DisplayName("Nombre")]
+        public string name { get; set;}
+
+        [Required]
+        [DisplayName("Duración")]
+        public string duration { get; set; }
+
+        [Required]
+        public TimeSpan durationSeconds { get; set; }
+
+        [Required]
+        [DisplayName("Intérprete")]
+        public string singer { get; set; }
+
+        [Required]
+        public string SoundPath;
+    }
+}
---
+++ Laboratorio0_1016816/Tools/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Laboratorio0_1016816.Models;
+using WMPLib;
+
+namespace Laboratorio0_1016816.Tools
+{
+    public class Singleton
+    {
+        public Dictionary<string, SongModel> Playlist = new Dictionary<string, SongModel>();
+        public List<SongModel> specialList = new List<SongModel>();
+        public WindowsMediaPlayer SoundPlayer = new WindowsMediaPlayer();
+        public int PositionActualSong=-1;
+        private static Singleton MainData;
+        public static Singleton Data
+        {
+            get
+            {
+                if (MainData == null)
+                {
+                    MainData = new Singleton();
+                }
+                return MainData;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
